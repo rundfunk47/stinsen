2dc2c2aded09f0bc34f221edd053ad3cefa1391d
Narek Mailian <narek.mailian@gmail.com>
2021-06-27 09:37:53 +0200

    Add customize-option for ViewCoordinatable

--- Sources/Stinsen/ViewCoordinatable/ViewCoordinatable.swift
+++ Sources/Stinsen/ViewCoordinatable/ViewCoordinatable.swift
@@ -4,10 +4,12 @@ import SwiftUI
 ///The ViewCoordinatable represents a view with routes that can be switched to but not pushed or presented modally. This can be used if you have a need to switch between different "modes" in the app, for instance if you switch between logged in and logged out. The ViewCoordinatable will recreate the view and the coordinator, so it is not suited to replace a tab-bar or similar modes of navigation, where you want to preserve the state.
 public protocol ViewCoordinatable: Coordinatable {
     associatedtype Route: ViewRoute
+    associatedtype CustomizeViewType: View
     associatedtype Start: View
     func resolveRoute(route: Route) -> AnyCoordinatable
     @ViewBuilder func start() -> Start
     var children: ViewChild { get }
+    func customize(_ view: AnyView) -> CustomizeViewType
 }
 
 public extension ViewCoordinatable {
@@ -25,8 +27,12 @@ public extension ViewCoordinatable {
         
     func coordinatorView() -> AnyView {
         return AnyView(
-            ViewCoordinatableView(coordinator: self)
+            ViewCoordinatableView(coordinator: self, customize: customize)
         )
+    }
+    
+    func customize(_ view: AnyView) -> some View {
+        return view
     }
     
     func dismissChildCoordinator(_ childCoordinator: AnyCoordinatable, _ completion: (() -> Void)?) {
--- Sources/Stinsen/ViewCoordinatable/ViewCoordinatableView.swift
+++ Sources/Stinsen/ViewCoordinatable/ViewCoordinatableView.swift
@@ -1,27 +1,31 @@
 import Foundation
 import SwiftUI
 
-struct ViewCoordinatableView<T: ViewCoordinatable>: View {
+struct ViewCoordinatableView<T: ViewCoordinatable, U: View>: View {
     var coordinator: T
     let router: ViewRouter<T.Route>
     @ObservedObject var children: ViewChild
-    
-    init(coordinator: T) {
+    private var customize: (AnyView) -> U
+
+    init(coordinator: T, customize: @escaping (AnyView) -> U) {
         self.router = ViewRouter(coordinator)
+        self.customize = customize
         self.coordinator = coordinator
         self.children = coordinator.children
     }
         
     var body: some View {
-        Group {
-            if let childCoordinator = coordinator.childCoordinators.first {
-                childCoordinator
-                    .coordinatorView()
-            } else {
-                coordinator
-                    .start()
-            }
-        }
+        customize(
+            AnyView(
+                Group {
+                    if let childCoordinator = coordinator.childCoordinators.first {
+                        childCoordinator.coordinatorView()
+                    } else {
+                        coordinator.start()
+                    }
+                }
+            )
+        )
         .environmentObject(router)
     }
 }
