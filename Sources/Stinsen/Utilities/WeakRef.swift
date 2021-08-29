ac7e0b6d468d291f968fe51154053c1b4ed132ad
Narek Mailian <narek.mailian@gmail.com>
2021-08-29 17:34:37 +0200

    Update WeakRef

--- Sources/Stinsen/Utilities/WeakRef.swift
+++ Sources/Stinsen/Utilities/WeakRef.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 // see https://swiftrocks.com/weak-dictionary-values-in-swift
-final class WeakRef {
-    weak var value: AnyObject?
+final class WeakRef<T: AnyObject> {
+    weak var value: T?
+    
+    init(value: T) {
+        self.value = value
+    }
 }
