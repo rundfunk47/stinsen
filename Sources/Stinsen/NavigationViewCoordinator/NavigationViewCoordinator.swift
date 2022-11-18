391ef10b56ee3ea850bc9dfdbe5be96eb81e2c9e
Hunor Andras <hunor.andras@cognitivecreators.com>
2022-11-18 13:14:47 +0200

    Added ModalNavigationViewCoordinator class for modal presentation wrapper. Reverted original NavigationViewCoordinator to make pushes work.

--- Sources/Stinsen/NavigationViewCoordinator/NavigationViewCoordinator.swift
+++ Sources/Stinsen/NavigationViewCoordinator/NavigationViewCoordinator.swift
@@ -13,6 +13,32 @@ public class NavigationViewCoordinator<T: Coordinatable>: ViewWrapperCoordinator
             )
             #else
             AnyView(
+                NavigationView {
+                    view
+                }
+                .navigationViewStyle(StackNavigationViewStyle())
+            )
+            #endif
+        }
+    }
+    
+    @available(*, unavailable)
+    public override init(_ childCoordinator: T, _ view: @escaping (AnyView) -> AnyView) {
+        fatalError("view cannot be customized")
+    }
+}
+
+public class ModalNavigationViewCoordinator<T: Coordinatable>: ViewWrapperCoordinator<T, AnyView> {
+    public init(_ childCoordinator: T) {
+        super.init(childCoordinator) { view in
+            #if os(macOS)
+            AnyView(
+                NavigationView {
+                    view
+                }
+            )
+            #else
+            AnyView(
                 NavigationViewBuilder.versionBasedWrapper(view: view)
             )
             #endif
